c5ae6ce0bec896ff9a3c4d863652290aa9dea5ca
Mattia Monga <mattia.monga@unimi.it>
2025-06-11 10:22:50 +0200

    Solution

--- exam.py
+++ exam.py
@@ -32,7 +32,7 @@
 
 # **SOLVE EACH EXERCISE IN ONE OR MORE NOTEBOOK CELLS AFTER THE QUESTION.**
 
-import numpy as np
+import numpy as np   
 import pandas as pd  # type: ignore
 import matplotlib.pyplot as plt # type: ignore
 import pymc as pm   # type: ignore
@@ -52,14 +52,18 @@ import arviz as az  # type: ignore
 # Read the data in a pandas DataFrame. Be sure  that the columns `Date` and `DateBorn` has dtype `pd.datetime64[ns]`.
 #
 
-pass
+data = pd.read_csv('rhinos.csv', sep=';', parse_dates=['Date', 'DateBorn'], dayfirst=True)
+data.dtypes
+
+data.head()
 
 # ### Exercise 2 (max 3 points)
 #
 # Add a column `Age` with the age in weeks of the rhinos at the time of the sighting.
 #
 
-pass
+data['Age'] = (data['Date'] - data['DateBorn']).astype(int) / (7*24*60*60*10**9)
+
 
 # ### Exercise 3 (max 7 points)
 #
@@ -68,31 +72,79 @@ pass
 #
 # To get the full marks, you should declare correctly the type hints and add a test within a doctest string.
 
-pass
+def dehornings(sightings: list[str]) -> int:
+    """Return how many times a rhino was dehorned.
+
+    >>> dehornings(["Horned", "Horned", "Dehorned"])
+    1
+
+    >>> dehornings(["Dehorned", "Dehorned", "Dehorned"])
+    1
+
+    >>> dehornings(["Horned", "Horned", "Horned", "Horned"])
+    0
+
+    >>> dehornings(["Dehorned", "Horned", "Dehorned"])
+    2
+
+    >>> dehornings(["Dehorned"])
+    1
+    """
+    if sightings == [] or sightings.count("Dehorned") == 0:
+        return 0
+    dehorned = sightings.index("Dehorned")
+    i = dehorned + 1
+    while i < len(sightings) and sightings[i] == "Dehorned":
+        i = i + 1
+    if i == len(sightings):
+        return 1
+    return 1 + dehornings(sightings[i+1:])
+
+
+import doctest
+doctest.testmod()
 
 # ### Exercise 4 (max 5 points)
 #
 # Apply the function defined in Exercise 3 to the data referring to the rhinos and find the rhino that was dehorned the most. Please note that you should order the list of sightings by `Date`.
 
-pass
+data.sort_values(by='Date').groupby('RhinosAtSighting')['Horn'].apply(lambda s: dehornings(s.tolist())).sort_values(ascending=False).head(1)
 
 # ### Exercise 5 (max 2 points)
 #
 # Compute for each rhino the weeks between the first and the last sighting.
 
-pass
+data.sort_values(by='Date').groupby('RhinosAtSighting')['Age'].apply(lambda s: s.iloc[-1] - s.iloc[0])
 
 # ### Exercise 6 (max 4 points)
 #
 # Plot a histogram of the number of rhinos observed in each reserve
 
-pass
+# +
+nums = data.groupby('Reserve')['RhinosAtSighting'].count()
+
+fig, ax = plt.subplots(1)
+ax.bar(nums.index, nums, label='Number of rhinos')
+ax.set_xticks(ax.get_xticks(), ax.get_xticklabels(), rotation=45, ha='right') # type: ignore
+_ = ax.legend()
+# -
 
 # ### Exercise 7 (max 4 points)
 #
 # Plot together the histograms of the number of male and female rhinos observed in each reserve
 
-pass
+# +
+nums_f = data[data['Sex'] == 'Female'].groupby('Reserve')['RhinosAtSighting'].nunique()
+nums_m = data[data['Sex'] == 'Male'].groupby('Reserve')['RhinosAtSighting'].nunique()
+
+
+fig, ax = plt.subplots(1)
+ax.bar(range(len(nums_f)), nums_f, width=-.4, align='edge', label='Number of female rhinos')
+ax.bar(range(len(nums_m)), nums_m, width=.4, align='edge', label='Number of male rhinos')
+
+ax.set_xticks(range(len(nums)), nums.index, rotation=45, ha='right')
+_ = ax.legend()
+# -
 
 # ### Exercise 8 (max 5 points)
 #
@@ -104,9 +156,21 @@ pass
 # - the observed mean number of dehorned sightings for each `Reserve` is normally distributed with a standard deviation of $\sigma$ and a mean given by $\alpha + \beta\cdot N$, where $N$ is number of (unique) rhinos in the reserve.
 #
 # Code this model with pymc, sample the model, and plot the summary of the resulting estimation by using `az.plot_posterior`.
-#
-#
-#
-#
 
-pass
+# +
+N = data.groupby('Reserve')['RhinosAtSighting'].nunique()
+mean_dehornings = data.groupby('Reserve')['Horn'].apply(lambda s: (s == 'Dehorned').mean()) * N
+
+with pm.Model() as m:
+    a = pm.Normal('alpha', 0, 2)
+    b = pm.Normal('beta', 0, 2)
+    s = pm.Exponential('sigma', 1)
+    pm.Normal('age', a + b*N, s, observed=mean_dehornings)
+
+    idata = pm.sample()
+# -
+
+
+_ = az.plot_posterior(idata)
+
+
